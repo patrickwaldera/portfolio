fd9d4804eb7944877cc3730d4eb301df9c095752
patrickwaldera <patrickwaldera@gmail.com>
2024-06-22 12:14:56 -0300

    chore: atualizar descrição

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -18,8 +18,8 @@ const About = () => {
             <div className="info-txt">
                 <h1>Patrick Waldera</h1>
                 <InfoDiv>
-                    <h3>Desenvolvedor</h3>
-                    <p>Sou Desenvolvedor Full Stack Júnior, buscando crescimento profissional e experiências para aperfeiçoar minhas habilidades. Estou motivado a superar desafios e contribuir com soluções inovadoras.</p>
+                    <h3>Desenvolvedor de Software</h3>
+                    <p>Sou Desenvolvedor Full Stack, dedicado ao crescimento profissional contínuo e ao aperfeiçoamento de minhas habilidades. Com uma forte motivação para enfrentar desafios, estou sempre em busca de oportunidades para contribuir com soluções inovadoras e agregar valor aos projetos em que participo.</p>
                 </InfoDiv>
                 <ButtonsContainer>
                     <IconButton link="https://github.com/patrickwaldera" target={'_blank'} image={github} altText='Github'> </IconButton>
