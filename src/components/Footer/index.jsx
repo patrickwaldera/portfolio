6883535381ea710496eece3f7aa0fb5df93c5498
patrickwaldera <20331609+patrickwaldera@users.noreply.github.com>
2025-02-19 19:02:03 -0300

    fix: corrigir ano

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,46 +1,46 @@
-import { 
+import {
   FooterContainer,
   FootertTitle,
   FooterNav,
   ButtonsContainer
-} from './styles'
+} from './styles';
 
-import { IconButton } from '../IconButton'
+import { IconButton } from '../IconButton';
 import { Link } from "react-scroll";
-import github from '../../assets/github.png'
-import linkedin from '../../assets/linkedin.png'
+import github from '../../assets/github.png';
+import linkedin from '../../assets/linkedin.png';
 
 const Footer = () => {
-
+  const year = new Date().getFullYear();
   return (
     <FooterContainer>
-        <FootertTitle>
-          <h6>Patrick Waldera</h6>
-        </FootertTitle>
-        <FooterNav>
-          <Link className='nav-links' to="inicio" smooth={true} offset={-70} duration={500}>
-            Início
-          </Link>
-          <Link className='nav-links' to="habilidades" smooth={true} offset={-70} duration={500}>
-            Habilidades
-          </Link>
-          <Link className='nav-links' to="projetos" smooth={true} offset={-70} duration={500}>
-            Projetos
-          </Link>
-          <Link className='nav-links' to="qualificacoes" smooth={true} offset={-70} duration={500}>
-            Qualificações
-          </Link>
-          <Link className='nav-links' to="contato" smooth={true} offset={-70} duration={500}>
-            Contato
-          </Link>
-        </FooterNav>
-        <ButtonsContainer>
-          <IconButton link="https://github.com/patrickwaldera" target={'_blank'} image={github} altText='Github'> </IconButton>
-          <IconButton link="https://www.linkedin.com/in/patrickwaldera/" target={'_blank'} image={linkedin} altText='Linkedin'> </IconButton>
-       </ButtonsContainer> 
-       <p>© 2023</p>  
+      <FootertTitle>
+        <h6>Patrick Waldera</h6>
+      </FootertTitle>
+      <FooterNav>
+        <Link className='nav-links' to="inicio" smooth={true} offset={-70} duration={500}>
+          Início
+        </Link>
+        <Link className='nav-links' to="habilidades" smooth={true} offset={-70} duration={500}>
+          Habilidades
+        </Link>
+        <Link className='nav-links' to="projetos" smooth={true} offset={-70} duration={500}>
+          Projetos
+        </Link>
+        <Link className='nav-links' to="qualificacoes" smooth={true} offset={-70} duration={500}>
+          Qualificações
+        </Link>
+        <Link className='nav-links' to="contato" smooth={true} offset={-70} duration={500}>
+          Contato
+        </Link>
+      </FooterNav>
+      <ButtonsContainer>
+        <IconButton link="https://github.com/patrickwaldera" target={'_blank'} image={github} altText='Github'> </IconButton>
+        <IconButton link="https://www.linkedin.com/in/patrickwaldera/" target={'_blank'} image={linkedin} altText='Linkedin'> </IconButton>
+      </ButtonsContainer>
+      <p>© {year}</p>
     </FooterContainer>
-  )
-}
+  );
+};
 
-export { Footer }
+export { Footer };
